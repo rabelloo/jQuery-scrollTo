40b26744020c9979c6299377128fda5301ef68b1
André Luiz Rabêllo <andre.luiz_r@hotmail.com>
2016-08-03 15:35:04 -0300

    Create scrollTo.js

---
+++ scrollTo.js
@@ -0,0 +1,56 @@
+/*
+    Scrolling to elements made easy
+    
+    Author: André Luiz Rabêllo
+    Version: 1.0.0
+*/
+;(function($) {
+  
+  // Define function
+  function scrollTo (selector, duration) {
+    // Settings
+    var settings = $.extend({
+      selector: this,
+      duration: 500,
+      margin: 0
+    }, $.isPlainObject(selector) ? selector : {});
+    
+    // Overload check
+    if (!$.isPlainObject(selector) && selector !== undefined) {
+      // Both params overload
+      if ($.isNumeric(duration)) {
+        settings.selector = selector;
+        settings.duration = duration;
+      }
+      else if (duration === undefined) {
+        // Only duration overload
+        if ($.isNumeric(selector))
+          settings.duration = selector;
+        // Only selector overload
+        else
+          settings.selector = selector;
+      }
+    }
+
+    // Get offset
+    var offset = $(settings.selector).offset();
+
+    // Check for empty jQuery objects
+    if (!offset)
+        return this;
+
+    // Animate scroll
+    $('html, body').animate({
+        scrollTop: offset.top
+    }, +settings.duration >= 0 ? settings.duration : 500);
+
+    return this;
+  };
+  
+  // Check for .scrollTo existence (avoid conflicts)
+  if ($.fn.scrollTo === undefined)
+    $.fn.scrollTo = scrollTo;
+  else
+    $.fn.scrollToElement = scrollTo;
+  
+})(jQuery);
